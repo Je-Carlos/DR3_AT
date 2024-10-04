236f556fe1403c1d7aa438a4edce152ae5d68c13
JeanCarlos <dev.jeancotillo@gmail.com>
2024-10-04 13:24:13 -0300

    android

--- android/react-settings-plugin/build.gradle.kts
+++ android/react-settings-plugin/build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.9.24"
     id("java-gradle-plugin")
-    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 repositories {
@@ -17,26 +16,4 @@ gradlePlugin {
             implementationClass = "expo.plugins.ReactSettingsPlugin"
         }
     }
-}
-plugins {
-  id("com.android.application")
-
-  // Add the Google services Gradle plugin
-  id("com.google.gms.google-services")
-
-  ...
-}
-
-dependencies {
-  // Import the Firebase BoM
-  implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
-
-
-  // TODO: Add the dependencies for Firebase products you want to use
-  // When using the BoM, don't specify versions in Firebase dependencies
-  implementation("com.google.firebase:firebase-analytics")
-
-
-  // Add the dependencies for any other desired Firebase products
-  // https://firebase.google.com/docs/android/setup#available-libraries
 }
